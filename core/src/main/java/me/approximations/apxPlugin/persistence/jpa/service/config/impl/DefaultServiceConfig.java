9a19f36829260d594e1783e80d4f70169cdac46f
Approximations <datkidbosst@gmail.com>
2024-03-13 15:00:33 -0300

    feat: rename service threads

--- core/src/main/java/me/approximations/apxPlugin/persistence/jpa/service/config/impl/DefaultServiceConfig.java
+++ core/src/main/java/me/approximations/apxPlugin/persistence/jpa/service/config/impl/DefaultServiceConfig.java
@@ -1,5 +1,6 @@
 package me.approximations.apxPlugin.persistence.jpa.service.config.impl;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import me.approximations.apxPlugin.persistence.jpa.service.config.ServiceConfig;
 
 import java.util.concurrent.ExecutorService;
@@ -7,7 +8,7 @@ import java.util.concurrent.Executors;
 
 public class DefaultServiceConfig implements ServiceConfig {
     public static final DefaultServiceConfig INSTANCE = new DefaultServiceConfig();
-    public static final ExecutorService EXECUTOR_SERVICE = Executors.newCachedThreadPool();
+    public static final ExecutorService EXECUTOR_SERVICE = Executors.newCachedThreadPool(new ThreadFactoryBuilder().setNameFormat("ApxPlugin-Service-Thread-%d").build());
 
     @Override
     public ExecutorService getExecutorService() {
