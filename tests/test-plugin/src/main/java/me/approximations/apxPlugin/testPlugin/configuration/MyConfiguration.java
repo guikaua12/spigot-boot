2706724736e6142785122683e08b00aafd87fa46
Guilherme Silva <guilherme.silva@otisoftware.com.br>
2025-06-15 18:57:36 -0300

    refactor: delete unused annotations

---
+++ tests/test-plugin/src/main/java/me/approximations/apxPlugin/testPlugin/configuration/MyConfiguration.java
@@ -0,0 +1,7 @@
+package me.approximations.apxPlugin.testPlugin.configuration;
+
+import me.approximations.apxPlugin.core.context.configuration.annotations.Configuration;
+
+@Configuration
+public class MyConfiguration {
+}
