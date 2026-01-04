120ba931a91141240c7de6b1564885941f0e3841
guikaua12 <guilhermeksilva556@gmail.com>
2026-01-04 15:31:42 -0300

    feat: creation a custom injector for injecting ConfigRef<?>

--- config-spigot/src/main/java/tech/guilhermekaua/spigotboot/config/spigot/configuration/ConfigConfiguration.java
+++ config-spigot/src/main/java/tech/guilhermekaua/spigotboot/config/spigot/configuration/ConfigConfiguration.java
@@ -3,6 +3,7 @@ package tech.guilhermekaua.spigotboot.config.spigot.configuration;
 import org.bukkit.plugin.Plugin;
 import tech.guilhermekaua.spigotboot.config.spigot.SpigotConfigManager;
 import tech.guilhermekaua.spigotboot.config.spigot.injector.ConfigCollectionInjector;
+import tech.guilhermekaua.spigotboot.config.spigot.injector.ConfigRefInjector;
 import tech.guilhermekaua.spigotboot.core.context.annotations.Bean;
 import tech.guilhermekaua.spigotboot.core.context.annotations.Configuration;
 import tech.guilhermekaua.spigotboot.core.context.dependency.injector.CustomInjectorRegistryCustomizer;
@@ -17,8 +18,8 @@ public class ConfigConfiguration {
     @Bean
     public CustomInjectorRegistryCustomizer configCollectionInjector(SpigotConfigManager configManager) {
         return (registry) -> {
-            ConfigCollectionInjector collectionInjector = new ConfigCollectionInjector(configManager);
-            registry.register(collectionInjector);
+            registry.register(new ConfigCollectionInjector(configManager));
+            registry.register(new ConfigRefInjector(configManager));
         };
     }
 }
