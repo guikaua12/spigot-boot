34f45fbe8e4f8e040896af0f3175d7c89b3cc2fb
Guilherme Silva <guilherme.silva@otisoftware.com.br>
2026-03-14 19:31:52 -0300

    refactor: add @FunctionalInterface to CommandCompletionRegistryCustomizer

--- commands/src/main/java/tech/guilhermekaua/spigotboot/commands/CommandCompletionRegistryCustomizer.java
+++ commands/src/main/java/tech/guilhermekaua/spigotboot/commands/CommandCompletionRegistryCustomizer.java
@@ -1,5 +1,6 @@
 package tech.guilhermekaua.spigotboot.commands;
 
+@FunctionalInterface
 public interface CommandCompletionRegistryCustomizer {
     void customize(CommandCompletionRegistry registry);
 }
