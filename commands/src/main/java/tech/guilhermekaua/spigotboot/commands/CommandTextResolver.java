5520792b7e30047e39247e91e046711de2890754
Guilherme Silva <guilherme.silva@otisoftware.com.br>
2026-03-14 19:31:38 -0300

    refactor: add @FunctionalInterface to CommandTextResolver

--- commands/src/main/java/tech/guilhermekaua/spigotboot/commands/CommandTextResolver.java
+++ commands/src/main/java/tech/guilhermekaua/spigotboot/commands/CommandTextResolver.java
@@ -1,5 +1,6 @@
 package tech.guilhermekaua.spigotboot.commands;
 
+@FunctionalInterface
 public interface CommandTextResolver {
     String resolve(CommandTextResolutionContext context, String value);
 }
